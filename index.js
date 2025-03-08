8c8d62902ea8161906f9ad40a8392943d1dd80b2
iqra <abdikarimiqra@gmail.com>
2025-03-08 21:10:17 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+function writeCards(namesArray, eventName) {
+  let messages = [];
+  
+  for (let i = 0; i < namesArray.length; i++) {
+    messages.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+  }
+
+  return messages;
+}
+
+// Example usage
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "surprise";
+console.log(writeCards(names, event));
+
+
+function countDown(startNumber) {
+    for (let i = startNumber; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  // Example usage
+  countDown(5);
+  
